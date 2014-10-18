c4d54dfa28120a2733dce672605acbd4729b27a5
Raj <raj_abc38@yahoo.com>
2014-10-18 18:13:11 +0530

    Manual redis settings for DataMapper

--- config/initializers/data_mapper.rb
+++ config/initializers/data_mapper.rb
@@ -1,1 +1,4 @@
-DataMapper.setup(:default, {:adapter  => "redis"})
+uri = URI.parse(ENV["REDISCLOUD_URL"] || "redis://localhost:6379/")
+options = {:adapter  => "redis", :host => uri.host, :port => uri.port, :password => uri.password}
+
+DataMapper.setup(:default, options)
