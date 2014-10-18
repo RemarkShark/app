a543a04886d52aae49a06a27e906066fcdbf75f4
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2014-10-18 10:54:14 +0530

    Init angular app

---
+++ app/assets/javascripts/annotatewithmeclient/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name annotatewithmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the annotatewithmeApp
+ */
+angular.module('annotatewithmeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
