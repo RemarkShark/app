7e0b2a4ee7c02a0a8f7740738d0e82a7019e2a9e
Raj <raj_abc38@yahoo.com>
2014-10-19 16:16:13 +0530

    Modified session create operation and associated URL

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,16 +5,10 @@ class Api::V1::SessionsController < ApplicationController
   param :img_src, String, "Direct Image URL for the session", :required => true
 
   def create
-    session = loop do
-      unique_url = Digest::SHA1.hexdigest([Time.now, rand].join)
+    session = Session.create(img_src: params[:img_src])
 
-      unless Session.exists?(unique_url)
-        break Session.create(uniq_hash: unique_url, img_src: params[:img_src])
-      end
-    end
-
-    resp = {id: session.id, url: BASE_URL + session.uniq_hash, :img_src => session.img_src}
-    respond_with(resp, :location => resp[:url])
+    session.path = "/sessions/#{session.id}"
+    respond_with(session, :location => api_v1_sessions_url(session))
   end
 
   api :DELETE, "/sessions/:id", "Destroy the URL (session)"
